7b7c8dfe199766179f32c270ec3229743e9693d2
lesnijahoda <honzabro137@seznam.cz>
2023-10-13 17:41:50 +0200

    novýobrázek+fixingtext

--- res/js/main.js
+++ res/js/main.js
@@ -8,6 +8,10 @@ const farmacounter = document.getElementById("farmacounter");
 const farmacounterlevel = document.getElementById("farmacounterlevel");
 const autoclickercounterlevel = document.getElementById("autoclickercounterlevel");
 const autoclickercounter = document.getElementById("autoclickercounter");
+const  mineUpgrade= document.getElementById("mineUpgrade");
+const  minecounter= document.getElementById("minecounter");
+const  minecounterlevel= document.getElementById("minecounterlevel");
+
 
 let numberOfCookies = 0;
 let costOfclickUpgrade = 20;
@@ -22,9 +26,12 @@ let farmaUpgradeIncrease = 0;
 let farmaUpgradeCost =  100000;
 let farma;
 let farmalevel =0;
-/*const updateinfo = () => {
-    
-}*/
+
+let mineUpgradeIncrease =0;
+let mineUpgradeCost = 1000000;
+let mine;
+let minelevel =0;
+
 
 banana.onmouseover = () => {
     console.log("banana mouse")
@@ -69,11 +76,11 @@ const clickUpgradeFunction = () => {
         numberOfCookies -= costOfclickUpgrade;
 
         //aktualizace odstavce
-        counter.innerText = "Cookies:" + numberOfCookies;
+        counter.innerText = "Cookies: " + numberOfCookies;
 
         //zvedneme pocet susenek na kliknuti
-        clickUpgradeIncrease++;
-        costOfclickUpgrade *= 2;
+        clickUpgradeIncrease+=10;
+        costOfclickUpgrade += 100;
         clickUpgrade.innerText = "Buy clickupgrade:" +costOfclickUpgrade;
     }
 };
@@ -93,7 +100,7 @@ clickUpgrade.onclick = clickUpgradeFunction;
 autoclickerUpgrade.onclick = () => {
     if (numberOfCookies >= autoclickerCost) {
         numberOfCookies -= autoclickerCost;
-        autoclickerCost *=2;
+        autoclickerCost +=500;
         autoclickerUpgrade.innerText = "Buy autoclicker upgrade:" +autoclickerCost;
         autoclickerlevel++;
         autoclickercounterlevel.innerText = "Autocliker level:" +autoclickerlevel;
@@ -101,8 +108,8 @@ autoclickerUpgrade.onclick = () => {
         //odecist sušenky
         //aktualizovat odstavec se susenky
         autoclickercounter.innerText= "Autoclicker price: " +autoclickerCost;
-        counter.innerText = "Cookies" + numberOfCookies;
-        autoclickIncrease++;
+        counter.innerText = "Cookies: " + numberOfCookies;
+        autoclickIncrease+=5;
         //clear
         clearInterval(autoclicker);
         //spustit interval
@@ -110,7 +117,7 @@ autoclickerUpgrade.onclick = () => {
             //zvednout o nějaké číslo
             numberOfCookies += autoclickIncrease;
             //aktualizovat odstavec
-            counter.innerText = "Cookies" + numberOfCookies;
+            counter.innerText = "Cookies: " + numberOfCookies;
             
         }, 1000);
         
@@ -135,7 +142,7 @@ farmaUpgrade.onclick = () => {
         //odecist sušenky
         //aktualizovat odstavec se susenky
         farmacounter.innerText= "Farm price: " +farmaUpgradeCost;
-        counter.innerText = "Cookies" + numberOfCookies;
+        counter.innerText = "Cookies: " + numberOfCookies;
         farmaUpgradeIncrease+=5000;
         //clear
         clearInterval(farma);
@@ -144,11 +151,36 @@ farmaUpgrade.onclick = () => {
             //zvednout o nějaké číslo
             numberOfCookies += farmaUpgradeIncrease;
             //aktualizovat odstavec
-            counter.innerText = "Cookies" + numberOfCookies;
+            counter.innerText = "Cookies: " + numberOfCookies;
             
         }, 1000);
         
     }
 }
 
-
+mineUpgrade.onclick = () => {
+    if (numberOfCookies >= mineUpgradeCost) {
+        numberOfCookies -= mineUpgradeCost;
+        mineUpgradeCost +=50000;
+        mineUpgrade.innerText = "Buy Farmaupgrade:" +mineUpgradeCost;
+        minelevel++;
+        minecounterlevel.innerText = "Farm level:" +minelevel;
+        //numberOfCookies = numberOfCookies - 100;        delší způsob
+        //odecist sušenky
+        //aktualizovat odstavec se susenky
+        minecounter.innerText= "Farm price: " +mineUpgradeCost;
+        counter.innerText = "Cookies: " + numberOfCookies;
+        mineUpgradeIncrease+=70000;
+        //clear
+        clearInterval(mine);
+        //spustit interval
+        mine = setInterval(() => {
+            //zvednout o nějaké číslo
+            numberOfCookies += mineUpgradeIncrease;
+            //aktualizovat odstavec
+            counter.innerText = "Cookies: " + numberOfCookies;
+            
+        }, 900);
+        
+    }
+}
